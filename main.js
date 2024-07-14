cbc16dd36a11fa8b035ae9ccf631779cb245a8e7
hikmot-kasali <kasalihikmot@gmail.com>
2024-07-14 20:06:12 +0800

    Adding

--- main.js
+++ main.js
@@ -1,34 +1,9 @@
-///document.getElementById("count-el").innerText = 5
-/*
-let myAge = 16
-let humanDogRatio = 7
-let myDogAge = myAge * humanDogRatio
-console.log(myDogAge)
 
-let bonusPoints = 50
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
-bonusPoints = bonusPoints - 75
-console.log(bonusPoints)
-bonusPoints = bonusPoints + 45
-console.log(bonusPoints)
-*/
-let count= document.getElementById("count-el")
-let incrementBtn = 0
+let count = 0
+let countEl = document.getElementById("count-el")
+
 function increment() {
-    incrementBtn = incrementBtn + 1
-    count.innerText = incrementBtn
-    console.log(incrementBtn)
+    count = count + 1
+    countEl.innerText = count
 }
-increment()
-
-let Multiples = document.getElementById("count-el")
-let fourIncrement = 0
-function fourMultiples(){
-    fourIncrement = fourIncrement + 4
-    Multiples.innerText = fourIncrement
-    console.log(fourIncrement)
-}
-fourMultiples()
-
 
---
+++ rough.js
@@ -0,0 +1,21 @@
+///document.getElementById("count-el").innerText = 5
+/*
+let myAge = 16
+let humanDogRatio = 7
+let myDogAge = myAge * humanDogRatio
+console.log(myDogAge)
+
+let bonusPoints = 50
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
+*/
+    <!--<button id="four-btn" onclick="fourMultiples()">Four-multiples</button>-->
+    <button id="save-btn" onclick= "save()" >Save</button>
+    let saveEl = document.getElementById("save-btn")
+function save(){
+    console.log(count)
+}
