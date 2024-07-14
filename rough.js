17a84e586b216c4e8604ee1046a80792c68c483e
hikmot-kasali <kasalihikmot@gmail.com>
2024-07-14 21:17:11 +0800

    Add

--- rough.js
+++ rough.js
@@ -14,7 +14,7 @@ bonusPoints = bonusPoints + 45
 console.log(bonusPoints)
 */
     <!--<button id="four-btn" onclick="fourMultiples()">Four-multiples</button>-->
-    <button id="save-btn" onclick= "save()" >Save</button>
+    <button id="save-btn" onclick= "save()" >Save</button>-->
     let saveEl = document.getElementById("save-btn")
 function save(){
     console.log(count)
